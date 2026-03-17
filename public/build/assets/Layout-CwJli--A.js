76997331f0404a030082215778f3ba71e9024cc3
Carlos Silveira <silveira.alberto24@gmail.com>
2026-03-17 17:07:20 -0600

    Fix raw HTML rendering in Article Show

--- public/build/assets/Layout-CwJli--A.js
+++
@@ -1,1 +0,0 @@
-import{j as r,a}from"./app-B4BD9TgA.js";import s from"./TopNav-DK3HHdzt.js";import{a as e}from"./Footer-Z28y3E8T.js";const f=({children:o})=>r("div",{className:"flex flex-col min-h-screen",children:[a(s,{}),a("main",{className:"flex-grow",children:o}),a(e,{})]});export{f as default};
